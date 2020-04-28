ba827edc2fd7b14296376f5c9f814a8cc49c50a4
FDvorak1979 <f.dvorak2@students.clark.edu>
2020-04-27 20:41:35 -0700

    saved to new repo

---
+++ Hello Frank.py
@@ -0,0 +1,19 @@
+"""
+CTEC 121
+Frank Dvorak
+Programming Lab I
+PRogramming Lab I / Print Command Lab
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print()
+    print("Salve")
+    print("Frank")
+    print()
+main()    
---
+++ I Esdras Chapter 4 Verse 35.py
@@ -0,0 +1,18 @@
+"""
+CTEC 121
+Frank Dvorak
+Programming Lab I
+PRogramming Lab I / Print Command Lab
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print()
+    print("But truth is greater and stronger than all of these things")
+    print()
+main()    
